fe0d999ad0fa9869ee9e6592db18926d007508f9
OTeeEnabor <tobienabor@gmail.com>
2019-09-12 20:44:54 +0200

    script to hold user details to log in to hotel db.

---
+++ user.php
@@ -0,0 +1,7 @@
+<?php 
+//connect to the hotelbooking  database
+$servername = 'localhost'; //using wampserver
+$username   ='adminhotel';
+$password   = 'hotelpass';
+$dbname     ='HOTELBOOKING';
+?>
